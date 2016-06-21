03219534d2b7e25931e42a1fc447ef7b27954dec
bilalbarki <bilalbarki@gmail.com>
2016-06-21 12:07:58 +0000

    added comments and header functionality

--- script.py
+++ script.py
@@ -9,30 +9,37 @@ id_number=40
 votes=98
 #configuration end
 
-#initialization start
-default_socket = socket.socket
+#tor socks 5 parameters
 SOCKS5_PROXY_HOST = 'localhost'
 SOCKS5_PROXY_PORT = 9050
-#initialization end
+#tor socks 5 parameters end
 
+#changes circuit upon each call and calls the generate post request function
 def use_tor():
-     socket.socket = default_socket
      controller.signal(Signal.NEWNYM)
      socks.set_default_proxy(socks.SOCKS5, SOCKS5_PROXY_HOST, SOCKS5_PROXY_PORT)
      socket.socket = socks.socksocket
      generate_vote()
 
+#makes a post request
 def generate_vote():
-     cj = cookielib.CookieJar()
+     cj = cookielib.CookieJar() #cookies will get stored in cj
      opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+     #add headers if the website checks it
+     opener.addheaders=[
+        ('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'),
+        ('Referer', 'url.com'),
+     ]
+     #open a session
      session = opener.open('url.com')
      if session.getcode() == 200:
+          #get the value of the session cookie
           for cookie in cj:
                if cookie.name=='session':
                     params = { 'id': id_number, 'submit': 'Submit', 'key':  cookie.value }
           url_params = urllib.urlencode(params) # url encode the parameters
-          post=opener.open('url.com', url_params)
-     opener.close()
+          post = opener.open('url.com', url_params) #make the post request
+     opener.close() #close the opener
 
 with Controller.from_port(port = 9051) as controller:
     controller.authenticate() #write password here if you set a password earlier
